3c6a5b768639368704b9f2c5862a1f7bf379c1a1
sara ibnabbad <saraibnabbad96@gmail.com>
2022-11-30 10:25:00 +0100

    seeder OK

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,13 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Category;
+use App\Models\Comment;
+use App\Models\CommentReply;
+use App\Models\Post;
+use App\Models\Setting;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +21,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        User::factory(10)->create();
+        Category::factory(10)->create();
+        Post::factory(10)->create();
+        Comment::factory(10)->create();
+        CommentReply::factory(10)->create();
+        // Setting::factory()->create();
     }
 }
