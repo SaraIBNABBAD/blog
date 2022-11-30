fe4f6aebccb2b62a221af831b38ad59493ff05bd
Hippolyte EZIAN <polarusdev@gmail.com>
2022-11-30 10:22:52 +0100

    Update migrations

--- database/migrations/2022_11_27_094511_create_categories_table.php
+++ database/migrations/2022_11_27_094511_create_categories_table.php
@@ -15,6 +15,7 @@ return new class extends Migration {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('label', 50);
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
